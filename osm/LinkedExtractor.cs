c3c66b5088d0f80b6e23349ce9642174de7cf87b
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-02-18 20:56:07 -0500

    store `linked` as ids

--- osm/LinkedExtractor.cs
+++ osm/LinkedExtractor.cs
@@ -6,24 +6,22 @@ namespace osm
 {
     class LinkedExtractor
     {
-        static readonly string _url = "https://www.openstreetmap.org/";
-
         static void Wikidata(TagsCollectionBase tags, OsmGrainLinked link)
         {
             if (tags.TryGetValue("wikidata", out var v) && Regex.IsMatch(v, @"^Q[1-9][0-9]*$")) {
-                link.wikidata = "https://www.wikidata.org/wiki/" + v;
+                link.wikidata = v;
             }
         }
 
         public static void Extract(Node node, OsmGrainLinked link)
         {
-            link.osm = _url + "node/" + node.Id.Value.ToString();
+            link.osm = "node/" + node.Id.Value.ToString();
             Wikidata(node.Tags, link);
         }
 
         public static void Extract(Way way, OsmGrainLinked link)
         {
-            link.osm = _url + "way/" + way.Id.Value.ToString();
+            link.osm = "way/" + way.Id.Value.ToString();
             Wikidata(way.Tags, link);
         }
     }
