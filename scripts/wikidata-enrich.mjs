ee70f54f9945243425bdf388500e2a007a901b7c
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-06-13 00:13:23 +0200

    add extended wikidata enrichment

--- scripts/wikidata-enrich.mjs
+++ scripts/wikidata-enrich.mjs
@@ -12,33 +12,84 @@ import {
   constructFromEntity,
   fetchFromWikidata,
   getEntityList,
-  OPTIONAL_DESCRIPTION,
-  OPTIONAL_GEONAMES,
-  OPTIONAL_IMAGE,
-  OPTIONAL_KEYWORDS,
-  OPTIONAL_MAPYCZ,
   OPTIONAL_NAME,
-  PREAMBLE_SHORT
+  OPTIONAL_DESCRIPTION,
+  OPTIONAL_GENRE,
+  OPTIONAL_INSTANCE,
+  OPTIONAL_FACILITY,
+  OPTIONAL_MOVEMENT,
+  OPTIONAL_ARCH_STYLE,
+  OPTIONAL_FIELD_WORK,
+  OPTIONAL_IMAGE,
+  OPTIONAL_EMAIL,
+  OPTIONAL_PHONE,
+  OPTIONAL_WEBSITE,
+  OPTIONAL_INCEPTION,
+  OPTIONAL_OPENING_DATE,
+  OPTIONAL_CAPACITY,
+  OPTIONAL_ELEVATION,
+  OPTIONAL_MINIMUM_AGE,
+  OPTIONAL_GEONAMES,
+  OPTIONAL_MAPYCZ,
+  OPTIONAL_COUNTRY,
+  OPTIONAL_STREET,
+  OPTIONAL_HOUSE,
+  OPTIONAL_POSTAL_CODE,
+  PREAMBLE_SHORT,
 } from "./wikidata.mjs"
 
 const wikidataQuery = (payload) => `${PREAMBLE_SHORT}
 CONSTRUCT {
   ?wikidataId
-    my:name ?name ;
-    my:description ?description ;
-    my:keywords ?keyword ;
-    my:image ?image ;
-    my:geonames ?geoNamesId ;
-    my:mapycz ?mapyCzId .
+    my:name ?name;
+    my:description ?description;
+    my:genre ?genre;
+    my:instance ?instance;
+    my:facility ?facility;
+    my:movement ?movement;
+    my:archStyle ?archStyle;
+    my:fieldWork ?fieldWork;
+    my:image ?image;
+    my:email ?email;
+    my:phone ?phone;
+    my:website ?website;
+    my:inception ?inception;
+    my:openingDate ?openingDate;
+    my:capacity ?capacity;
+    my:elevation ?elevation;
+    my:minimumAge ?minimumAge;
+    my:geonames ?geoNamesId;
+    my:mapycz ?mapyCzId;
+    my:country ?country;
+    my:street ?street;
+    my:house ?house;
+    my:postalCode ?postalCode.
 }
 WHERE {
 VALUES ?wikidataId { ${payload} }
 ${OPTIONAL_NAME}
 ${OPTIONAL_DESCRIPTION}
-${OPTIONAL_KEYWORDS}
+${OPTIONAL_GENRE}
+${OPTIONAL_INSTANCE}
+${OPTIONAL_FACILITY}
+${OPTIONAL_MOVEMENT}
+${OPTIONAL_ARCH_STYLE}
+${OPTIONAL_FIELD_WORK}
 ${OPTIONAL_IMAGE}
+${OPTIONAL_EMAIL}
+${OPTIONAL_PHONE}
+${OPTIONAL_WEBSITE}
+${OPTIONAL_INCEPTION}
+${OPTIONAL_OPENING_DATE}
+${OPTIONAL_CAPACITY}
+${OPTIONAL_ELEVATION}
+${OPTIONAL_MINIMUM_AGE}
 ${OPTIONAL_MAPYCZ}
 ${OPTIONAL_GEONAMES}
+${OPTIONAL_COUNTRY}
+${OPTIONAL_STREET}
+${OPTIONAL_HOUSE}
+${OPTIONAL_POSTAL_CODE}
 }`;
 
 function constructFromJson(json) {
@@ -74,7 +125,18 @@ async function wikidataEnrich() {
             "name": obj.name,
             "attributes.name": obj.name,
             "attributes.image": obj.image,
+            "attributes.email": obj.email,
+            "attributes.phone": obj.phone,
+            "attributes.website": obj.website,
+            "attributes.year": obj.year,
+            "attributes.capacity": obj.capacity,
+            "attributes.elevation": obj.elevation,
+            "attributes.minimumAge": obj.minimumAge,
             "attributes.description": obj.description,
+            "attributes.address.country": obj.country,
+            "attributes.address.place": obj.street,
+            "attributes.address.house": obj.house,
+            "attributes.address.postalCode": obj.postalCode,
             "linked.mapycz": obj.mapycz,
             "linked.geonames": obj.geonames
           },
--- scripts/wikidata.mjs
+++ scripts/wikidata.mjs
@@ -7,6 +7,7 @@ const WIKIDATA_JSONLD_CONTEXT = {
   "my": "http://example.com/",
   "wd": "http://www.wikidata.org/entity/",
   "geo": "http://www.opengis.net/ont/geosparql#",
+  "xsd": "http://www.w3.org/2001/XMLSchema#",
   "name": {
     "@id": "my:name",
     "@container": "@language"
@@ -19,19 +20,84 @@ const WIKIDATA_JSONLD_CONTEXT = {
     "@id": "my:description",
     "@container": "@language"
   },
-  "keywords": {
-    "@id": "my:keywords",
+  "genre": {
+    "@id": "my:genre",
+    "@container": "@language"
+  },
+  "instance": {
+    "@id": "my:instance",
+    "@container": "@language"
+  },
+  "facility": {
+    "@id": "my:facility",
+    "@container": "@language"
+  },
+  "movement": {
+    "@id": "my:movement",
+    "@container": "@language"
+  },
+  "archStyle": {
+    "@id": "my:archStyle",
+    "@container": "@language"
+  },
+  "fieldWork": {
+    "@id": "my:fieldWork",
     "@container": "@language"
   },
   "image": {
     "@id": "my:image",
     "@type": "@id"
   },
+  "email": {
+    "@id": "my:email",
+    "@type": "@id"
+  },
+  "phone": {
+    "@id": "my:phone"
+  },
+  "website": {
+    "@id": "my:website",
+    "@type": "@id"
+  },
+  "inception": {
+    "@id": "my:inception",
+    "@type": "xsd:dateTime"
+  },
+  "openingDate": {
+    "@id": "my:openingDate",
+    "@type": "xsd:dateTime"
+  },
+  "capacity": {
+    "@id": "my:capacity",
+    "@type": "xsd:decimal"
+  },
+  "elevation": {
+    "@id": "my:elevation",
+    "@type": "xsd:decimal"
+  },
+  "minimumAge": {
+    "@id": "my:minimumAge",
+    "@type": "xsd:decimal"
+  },
   "geonames": {
     "@id": "my:geonames"
   },
   "mapycz": {
     "@id": "my:mapycz"
+  },
+  "country": {
+    "@id": "my:country",
+    "@container": "@language"
+  },
+  "street": {
+    "@id": "my:street",
+    "@container": "@language"
+  },
+  "house": {
+    "@id": "my:house"
+  },
+  "postalCode": {
+    "@id": "my:postalCode"
   },
   "wikidata": "@id"
 };
@@ -77,15 +143,76 @@ export const OPTIONAL_DESCRIPTION = `OPTIONAL {
   FILTER(LANG(?description) = "en" && STRLEN(STR(?description)) > 0)
 }`;
 
-export const OPTIONAL_KEYWORDS = `OPTIONAL {
+export const OPTIONAL_GENRE = `OPTIONAL {
+  ?wikidataId wdt:P136 ?genreId.
+  ?genreId rdfs:label ?genre.
+  FILTER(LANG(?genre) = "en" && STRLEN(STR(?genre)) > 0)
+}`;
+
+export const OPTIONAL_INSTANCE = `OPTIONAL {
   ?wikidataId wdt:P31 ?instanceOf.
-  ?instanceOf rdfs:label ?keyword.
-  FILTER(LANG(?keyword) = "en" && STRLEN(STR(?keyword)) > 0)
+  ?instanceOf rdfs:label ?instance.
+  FILTER(LANG(?instance) = "en" && STRLEN(STR(?instance)) > 0)
+}`;
+
+export const OPTIONAL_FACILITY = `OPTIONAL {
+  ?wikidataId wdt:P912 ?facilityId.
+  ?facilityId rdfs:label ?facility.
+  FILTER(LANG(?facility) = "en" && STRLEN(STR(?facility)) > 0)
+}`;
+
+export const OPTIONAL_MOVEMENT = `OPTIONAL {
+  ?wikidataId wdt:P135 ?movementId.
+  ?movementId rdfs:label ?movement.
+  FILTER(LANG(?movement) = "en" && STRLEN(STR(?movement)) > 0)
+}`;
+
+export const OPTIONAL_ARCH_STYLE = `OPTIONAL {
+  ?wikidataId wdt:P149 ?archStyleId.
+  ?archStyleId rdfs:label ?archStyle.
+  FILTER(LANG(?archStyle) = "en" && STRLEN(STR(?archStyle)) > 0)
+}`;
+
+export const OPTIONAL_FIELD_WORK = `OPTIONAL {
+  ?wikidataId wdt:P101 ?fieldWorkId.
+  ?fieldWorkId rdfs:label ?fieldWork.
+  FILTER(LANG(?fieldWork) = "en" && STRLEN(STR(?fieldWork)) > 0)
 }`;
 
 export const OPTIONAL_IMAGE = `OPTIONAL {
   ?wikidataId wdt:P18 ?image.
-  FILTER(ISURI(?image))
+}`;
+
+export const OPTIONAL_EMAIL = `OPTIONAL {
+  ?wikidataId wdt:P968 ?email.
+}`;
+
+export const OPTIONAL_PHONE = `OPTIONAL {
+  ?wikidataId wdt:P1329 ?phone.
+}`;
+
+export const OPTIONAL_WEBSITE = `OPTIONAL {
+  ?wikidataId wdt:P856 ?website.
+}`;
+
+export const OPTIONAL_INCEPTION = `OPTIONAL {
+  ?wikidataId wdt:P571 ?inception.
+}`;
+
+export const OPTIONAL_OPENING_DATE = `OPTIONAL {
+  ?wikidataId wdt:P1619 ?openingDate.
+}`;
+
+export const OPTIONAL_CAPACITY = `OPTIONAL {
+  ?wikidataId wdt:P1083 ?capacity.
+}`;
+
+export const OPTIONAL_ELEVATION = `OPTIONAL {
+  ?wikidataId wdt:P2044 ?elevation.
+}`;
+
+export const OPTIONAL_MINIMUM_AGE = `OPTIONAL {
+  ?wikidataId wdt:P2899 ?minimumAge.
 }`;
 
 export const OPTIONAL_MAPYCZ = `OPTIONAL {
@@ -96,32 +223,88 @@ export const OPTIONAL_GEONAMES = `OPTIONAL {
   ?wikidataId wdt:P1566 ?geoNamesId.
 }`;
 
+export const OPTIONAL_COUNTRY = `OPTIONAL {
+  ?wikidataId wdt:P17 ?countryId.
+  ?countryId rdfs:label ?country.
+  FILTER(LANG(?country) = "en" && STRLEN(STR(?country)) > 0)
+}`;
+
+export const OPTIONAL_STREET = `OPTIONAL {
+  ?wikidataId wdt:P669 ?streetId.
+  ?streetId rdfs:label ?street.
+  FILTER(LANG(?street) = "en" && STRLEN(STR(?street)) > 0)
+}`;
+
+export const OPTIONAL_HOUSE = `OPTIONAL {
+  ?wikidataId wdt:P4856 ?house.
+}`;
+
+export const OPTIONAL_POSTAL_CODE = `OPTIONAL {
+  ?wikidataId wdt:P281 ?postalCode.
+}`;
+
+function handleEnArray(list) {
+  const arr = (a) => Array.isArray(a) ? a : [a];
+  list = list ?? { "en": [] };
+
+  return arr(list.en)
+    .map((instance) => instance.toLowerCase())
+    .map((instance) => isValidKeyword(instance) ? instance : undefined)
+    .filter((instance) => instance !== undefined);
+}
+
+function handleDate(value) {
+  const d = new Date(value).getFullYear();
+  return isNaN(d) ? undefined : d;
+}
+
+function handleNumber(value) {
+  const n = parseFloat(value);
+  return isNaN(n) ? undefined : n;
+}
+
 export const getEntityList = (json) => json["@graph"] ?? [];
 
 export function constructFromEntity(ent) {
   const obj = { };
-  const arr = (a) => Array.isArray(a) ? a : [a];
   const fst = (a) => Array.isArray(a) ? a[0] : a;
 
-  ent.keywords = ent.keywords ?? { "en": [] };
-
-  const keywords = arr(ent.keywords.en)
-    .map((keyword) => isValidKeyword(keyword) ? keyword : undefined)
-    .filter((keyword) => keyword !== undefined);
+  const keywords = []
+    .concat(handleEnArray(ent.genre))
+    .concat(handleEnArray(ent.instance))
+    .concat(handleEnArray(ent.facility))
+    .concat(handleEnArray(ent.movement))
+    .concat(handleEnArray(ent.archStyle))
+    .concat(handleEnArray(ent.fieldWork));
 
   obj.keywords = [...new Set(keywords)];
 
   // en-containers
   obj.name = fst(ent.name?.en);
+  obj.street = fst(ent.street?.en);
+  obj.country = fst(ent.country?.en);
   obj.description = fst(ent.description?.en);
 
-  // lists
+  // identifiers
+  obj.house = fst(ent.house);
+  obj.postalCode = fst(ent.postalCode);
   obj.image = fst(ent.image);
+  obj.email = fst(ent.email)?.substring(7); // mailto:
+  obj.phone = fst(ent.phone);
+  obj.website = fst(ent.website);
   obj.mapycz = fst(ent.mapycz);
   obj.geonames = fst(ent.geonames);
 
+  // date
+  obj.year = handleDate(fst(ent.inception)) ?? handleDate(fst(ent.openingDate));
+  
+  // numbers
+  obj.capacity = handleNumber(fst(ent.capacity));
+  obj.elevation = handleNumber(fst(ent.elevation));
+  obj.minimumAge = handleNumber(fst(ent.minimumAge));
+
   // existing
-  obj.wikidata = ent.wikidata.substring(3);
+  obj.wikidata = ent.wikidata.substring(3); // wd:
 
   return obj;
 }
