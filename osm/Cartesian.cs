6c30af1202964c136e5988bcbf5b9a01e4105552
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-06-17 02:15:58 +0200

    refactor cartesian ops

--- osm/Cartesian.cs
+++ osm/Cartesian.cs
@@ -4,6 +4,15 @@ namespace osm;
 
 internal static class Cartesian
 {
+    /// <summary>
+    /// Calcuate signed area of a closed polygon in the Euclidean space.
+    /// Positive area means that points are ordered counter-clockwise,
+    /// negative otherwise.
+    /// <list>
+    /// <item>http://paulbourke.net/geometry/polygonmesh/</item>
+    /// </list>
+    /// </summary>
+    /// <param name="polygon">Closed polygon (first and last points are identical!).</param>
     private static double SignedArea(List<Point> polygon)
     {
         double ar = 0.0;
@@ -24,6 +33,13 @@ internal static class Cartesian
 
     public static bool IsCounterClockwise(List<Point> polygon) => SignedArea(polygon) > 0.0;
 
+    /// <summary>
+    /// Calculate the centroid of a closed polygon in the Euclidean space.
+    /// <list>
+    /// <item>http://paulbourke.net/geometry/polygonmesh/</item>
+    /// </list>
+    /// </summary>
+    /// <param name="polygon">Closed polygon (first and last points are identical!).</param>
     public static Point Centroid(List<Point> polygon)
     {
         double ar = 0.0, cx = 0.0, cy = 0.0;
@@ -42,6 +58,8 @@ internal static class Cartesian
             cx += (x0 + x1) * im;
             cy += (y0 + y1) * im;
         }
+
+        // ar = 2*area ~> 3.0 instead of 6.0
 
         return new() { lon = cx / (3.0 * ar), lat = cy / (3.0 * ar) };
     }
